a5f1a59ed93d8aba5659a2c34814ca03f8bd9b5a
Stepan Eremin <cmenaa2301@gmai.com>
2026-06-11 15:06:54 +0300

    Server complite

---
+++ Server/server.cpp
@@ -0,0 +1,52 @@
+#include "server.h"
+
+server::server()
+{
+    if(this -> listen(QHostAddress::Any, 2323)) // 1. Принимает сигналы с любого адреса 2. Порт прослушки
+    {
+        qDebug() << "ready to start";
+    }
+    else
+    {
+        qDebug() << errorString();
+
+    }
+}
+
+void server::incomingConnection(qintptr socketDescriptor)
+{
+    socket = new QTcpSocket;
+    socket -> setSocketDescriptor(socketDescriptor);
+    connect(socket, &QTcpSocket::readyRead, this, &server::slotReadyRead);
+    connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
+
+    Sockets.push_back(socket);
+    qDebug() << "client connected" << socketDescriptor;
+}
+
+void server::slotReadyRead()
+{
+    socket = (QTcpSocket*)sender();
+    QDataStream in(socket);
+    in.setVersion(QDataStream::Qt_6_9);
+    if(in.status() == QDataStream::Ok)
+    {
+        qDebug() << "ready...";
+        QString str;
+        in >> str;
+        qDebug() << str;
+    }
+    else
+    {
+        qDebug() << "DataStream Error";
+    }
+}
+
+void server::SendToClient(QString str)
+{
+    Data.clear();
+    QDataStream out(&Data, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_6_9);
+    out << str;
+    socket -> write(Data);
+}
